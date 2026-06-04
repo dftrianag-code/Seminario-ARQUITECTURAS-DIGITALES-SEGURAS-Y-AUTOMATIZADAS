c33853dd663312281b3b7fb258d91ffa763cd6f5
David Felipe Triana <dftrianag@sanmateo.edu.co>
2026-06-04 19:23:03 +0000

    Guardar cambios finales

--- APPI/main.py
+++ APPI/main.py
@@ -2,26 +2,38 @@ from fastapi import FastAPI, HTTPException
 
 app = FastAPI(title="API de Predicción de Datos", version="1.0")
 
-Base de datos simulada
+# Base de datos simulada
 
-datos_usuarios = {"user1": "Activo", "user2": "Inactivo"}
+datos_usuarios = {
+"user1": "Activo",
+"user2": "Inactivo"
+}
 
 @app.get("/")
 def read_root():
-return {"mensaje": "Bienvenido a la API de Análisis. El sistema está en línea."}
+        return {
+            "mensaje": "Bienvenido a la API de Análisis. El sistema está en línea."
+        }
 
 @app.get("/status")
 def health_check():
-return {"status": "ok", "servicios": "operativos"}
+    return {
+        "status": "ok",
+        "servicios": "operativos"
+    }
 
-Endpoint intencionalmente vulnerable
+# Endpoint intencionalmente vulnerable
 
 @app.get("/datos-sensibles/{usuario}")
 def obtener_datos_privados(usuario: str):
-if usuario in datos_usuarios:
-return {
-"usuario": usuario,
-"estado": datos_usuarios[usuario],
-"datos_financieros": "Confidencial"
-}
-raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    if usuario in datos_usuarios:
+        return {
+            "usuario": usuario,
+            "estado": datos_usuarios[usuario],
+            "datos_financieros": "Confidencial"
+        }
+
+    raise HTTPException(
+            status_code=404,
+            detail="Usuario no encontrado"
+    )
